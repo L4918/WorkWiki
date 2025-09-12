71a82414735d65a1bf24fbdbd1cf4c71151d8a29
L4918 <978126810@qq.com>
2025-09-12 16:20:46 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/org/example/wiki/service/EbookService.java
+++ src/main/java/org/example/wiki/service/EbookService.java
@@ -1,10 +1,16 @@
 package org.example.wiki.service;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import org.example.wiki.domain.Ebook;
+import org.example.wiki.domain.EbookExample;
 import org.example.wiki.mapper.EbookMapper;
+import org.example.wiki.req.EbookReq;
+import org.example.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 //使用@Service 注解  将这个Service交给Spring来管理
@@ -14,7 +20,19 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
