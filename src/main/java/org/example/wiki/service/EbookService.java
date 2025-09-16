6570740d12e9dd59dda2b50484e7efb13d9afa98
L4918 <978126810@qq.com>
2025-09-16 17:28:29 +0800

    5-4 列表查询接口支持动态SQL name参数有值时按name查，没值时查全部 封面小图样式调整，改为正方形

--- src/main/java/org/example/wiki/service/EbookService.java
+++ src/main/java/org/example/wiki/service/EbookService.java
@@ -9,6 +9,7 @@ import org.example.wiki.resp.EbookResp;
 import org.example.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -24,7 +25,9 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+        if (!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
         List<EbookResp> respList = new ArrayList<>();
