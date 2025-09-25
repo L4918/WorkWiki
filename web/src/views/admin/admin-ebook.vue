10ab9ba41751c26c610fcd880edfb5d61d395b7e
L4918 <978126810@qq.com>
2025-09-25 11:37:57 +0800

    7-6 电子书列表显示分类名称

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -32,6 +32,9 @@
         <template #cover="{ text: cover }">
           <img v-if="cover" :src="cover" alt="avatar /">
         </template>
+        <template v-slot:category="{ text, record }">
+          <span>{{ getCategoryName(record.category1Id) }} / {{ getCategoryName(record.category2Id) }}</span>
+        </template>
         <template v-slot:action="{ text,record }">
           <a-space size="small">
             <a-button type="primary" @click="edit(record)">
@@ -114,13 +117,8 @@
           dataIndex:'name'
         },
         {
-          title: '分类一',
-          key:'category1Id',
-          dataIndex: 'category1Id'
-        },
-        {
-          title: '分类二',
-          dataIndex: 'category2Id'
+          title: '分类',
+          slots: { customRender: 'category' }
         },
         {
           title: '文档数',
@@ -243,6 +241,7 @@
       };
 
       const level1 = ref();
+      let categorys: any;
       /**
        * 查询所有分类
        */
@@ -252,7 +251,7 @@
           loading.value = false;
           const data = response.data;
           if (data.success){
-            const categorys = data.content;
+            categorys = data.content;
             console.log("原始数组:",categorys);
 
             level1.value = [];
@@ -262,7 +261,19 @@
             message.error(data.message);
           }
         });
-      }
+      };
+
+      const getCategoryName = (cid: number) => {
+        // console.log(cid)
+        let result = "";
+        categorys.forEach((item: any) => {
+          if (item.id === cid){
+            // return item.name; 注意，这里直接return不起作用
+            result = item.name;
+          }
+        });
+        return result;
+      };
 
       onMounted(() =>{
         handleQueryCategory();
@@ -280,6 +291,7 @@
         loading,
         handleTableChange,
         handleQuery,
+        getCategoryName,
 
         edit,
         add,
