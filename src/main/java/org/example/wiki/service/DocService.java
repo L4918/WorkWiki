f7796ede026fc4029c0680a3987cbecbeb107325
L4918 <978126810@qq.com>
2025-11-10 17:25:18 +0800

    10-2 阅读文档时,文档阅读数+1

--- src/main/java/org/example/wiki/service/DocService.java
+++ src/main/java/org/example/wiki/service/DocService.java
@@ -7,6 +7,7 @@ import org.example.wiki.domain.Doc;
 import org.example.wiki.domain.DocExample;
 import org.example.wiki.mapper.ContentMapper;
 import org.example.wiki.mapper.DocMapper;
+import org.example.wiki.mapper.DocMapperCust;
 import org.example.wiki.req.DocQueryReq;
 import org.example.wiki.req.DocSaveReq;
 import org.example.wiki.resp.DocQueryResp;
@@ -29,6 +30,10 @@ public class DocService {
 
     @Resource
     private DocMapper docMapper;
+
+    @Resource
+    private DocMapperCust docMapperCust;
+
     @Resource
     private ContentMapper contentMapper;
 
@@ -92,6 +97,8 @@ public class DocService {
         if (ObjectUtils.isEmpty(req.getId())){
             //新增
             doc.setId(snowFlake.nextId());
+            doc.setViewCount(0);
+            doc.setVoteCount(0);
             docMapper.insert(doc);
 
             content.setId(doc.getId());
@@ -119,6 +126,8 @@ public class DocService {
 
     public String findContent(Long id) {
         Content content = contentMapper.selectByPrimaryKey(id);
+        //文档阅读数+1
+        docMapperCust.increaseViewCount(id);
         if (ObjectUtils.isEmpty(content)){
             return "";
         }else {
