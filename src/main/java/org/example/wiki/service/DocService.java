31e6aa6a32907f92ad337966dcd3e6b46cf9199f
L4918 <978126810@qq.com>
2025-11-19 16:20:16 +0800

    10-9 完成点赞通知功能 点暂时,组装消息内容,网WS推送 前端收到WS消息后,弹出消息内容

--- src/main/java/org/example/wiki/service/DocService.java
+++ src/main/java/org/example/wiki/service/DocService.java
@@ -18,6 +18,7 @@ import org.example.wiki.util.CopyUtil;
 import org.example.wiki.util.RedisUtil;
 import org.example.wiki.util.RequestContext;
 import org.example.wiki.util.SnowFlake;
+import org.example.wiki.websocket.WebSocketServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -46,6 +47,9 @@ public class DocService {
 
     @Resource
     public RedisUtil redisUtil;
+
+    @Resource
+    public WebSocketServer webSocketServer;
 
     public List<DocQueryResp> all(Long ebookId) {
         DocExample docExample = new DocExample();
@@ -154,6 +158,10 @@ public class DocService {
         } else {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
         }
+
+        //推送消息
+        Doc docDb = docMapper.selectByPrimaryKey(id);
+        webSocketServer.sendInfo("【" + docDb.getName() + "】被点赞！");
     }
 
     public void updateEbookInfo(){
