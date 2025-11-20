01300fbacd68ec7898c62ca9cfe42f343459881f
L4918 <978126810@qq.com>
2025-11-20 15:17:14 +0800

    11-3 增加电子书快照表,生成持久层代码

---
+++ src/main/java/org/example/wiki/mapper/EbookSnapshotMapper.java
@@ -0,0 +1,30 @@
+package org.example.wiki.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.example.wiki.domain.EbookSnapshot;
+import org.example.wiki.domain.EbookSnapshotExample;
+
+public interface EbookSnapshotMapper {
+    long countByExample(EbookSnapshotExample example);
+
+    int deleteByExample(EbookSnapshotExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(EbookSnapshot record);
+
+    int insertSelective(EbookSnapshot record);
+
+    List<EbookSnapshot> selectByExample(EbookSnapshotExample example);
+
+    EbookSnapshot selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") EbookSnapshot record, @Param("example") EbookSnapshotExample example);
+
+    int updateByExample(@Param("record") EbookSnapshot record, @Param("example") EbookSnapshotExample example);
+
+    int updateByPrimaryKeySelective(EbookSnapshot record);
+
+    int updateByPrimaryKey(EbookSnapshot record);
+}
