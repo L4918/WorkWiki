99b0758ee15638c30c35d829a91bf1a61fcd242a
L4918 <978126810@qq.com>
2025-11-21 14:32:20 +0800

    11-6 后端增加获取统计数值接口

---
+++ src/main/java/org/example/wiki/controller/EbookSnapshotController.java
@@ -0,0 +1,28 @@
+package org.example.wiki.controller;
+
+import org.example.wiki.resp.CommonResp;
+import org.example.wiki.resp.StatisticResp;
+import org.example.wiki.service.EbookSnapshotService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook-snapshot")
+public class EbookSnapshotController {
+
+    @Resource
+    private EbookSnapshotService ebookSnapshotService;
+
+    @GetMapping("/get-statistic")
+    public CommonResp getStatistic() {
+        List<StatisticResp> statisticResp = ebookSnapshotService.getStatistic();
+        CommonResp<List<StatisticResp>> commonResp = new CommonResp<>();
+        commonResp.setContent(statisticResp);
+        return commonResp;
+    }
+
+}
--- src/main/java/org/example/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/org/example/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,6 +1,12 @@
 package org.example.wiki.mapper;
 
+import org.example.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 }
